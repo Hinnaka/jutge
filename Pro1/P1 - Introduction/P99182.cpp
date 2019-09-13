36ca0d682dca197a45866ba3f2f8868bcdbd8ba7
Alejandro Riba Palma <43859210+Hinnaka@users.noreply.github.com>
2019-09-14 00:29:51 +0200

    Add files via upload

---
+++ Pro1/P1 - Introduction/P99182.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	float x, y = 0;
+	cin>>x;
+	cin>>y;
+	double z =( (x + y) / 2 );
+	cout<< z <<endl;
+}
---
+++ Pro1/P1 - Introduction/X54725.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int x, y = 0;
+	cin>>x;
+	cin>>y;
+	if(x > y){
+		cout<<y<<endl;
+	}
+	if(x < y){
+		cout<<x<<endl;
+	}
+	if(x == y){
+		cout<<x<<endl;
+	}
+	
+	
+	
+}
